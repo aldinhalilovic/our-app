e7258187f2e8b2ae51212cff1941d8a77f7b1b70
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-09-27 16:40:26 +0200

    icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,7 +16,19 @@ function Navbar() {
             <h2> About us</h2>
           </NavLink>
           <NavLink to={"/cart"}>
-            <h2>Cart</h2>
+            <svg
+              class="h-8 w-8 text-red-900"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <circle cx="9" cy="21" r="1" /> <circle cx="20" cy="21" r="1" />{" "}
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+            </svg>
           </NavLink>
         </div>
       </div>
